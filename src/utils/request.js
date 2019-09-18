6821e0803be90422b1e44b9945ced4657cfb3a02
lipengzhou <lpzmail@163.com>
2019-09-18 15:22:16 +0800

    项目初始化-配置处理后端返回数据超出js安全整数范围

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,6 +2,7 @@
  * 请求模块：封装了 axios
  */
 import axios from 'axios'
+import JSONbig from 'json-bigint'
 
 // 创建 axios 实例
 // axios.create 的作用是克隆一个 axios 实例，它的作用和 axios 是一样的
@@ -12,6 +13,18 @@ import axios from 'axios'
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/'
 })
+
+// 配置处理后端返回的数据中包含超出 JavaScript 安全整数范围问题
+request.defaults.transformResponse = [function (data) {
+  // 假如 data 不是标准的 JSON 格式的字符串
+  try {
+    // data 数据可能不是标准的 JSON 格式字符串，否则会导致 JSONbig.parse(data) 转换失败报错
+    return JSONbig.parse(data)
+  } catch (err) {
+    // 无法转换的数据直接原样返回
+    return data
+  }
+}]
 
 // 配置 axios
 // 请求拦截器
