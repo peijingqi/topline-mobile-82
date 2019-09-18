0ef738ad55d654f466adbfe046baf41e25c9854c
lipengzhou <lpzmail@163.com>
2019-09-18 14:54:16 +0800

    项目初始化-导入Vant&配置按需加载

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 按需注册 Vant 组件
+import {
+  Button,
+  Cell,
+  CellGroup
+} from 'vant'
+
+Vue
+  .use(Button)
+  .use(Cell)
+  .use(CellGroup)
+
 Vue.config.productionTip = false
 
 new Vue({
