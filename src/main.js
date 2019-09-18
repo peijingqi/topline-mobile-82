d7d2b40aa6782bdaf0012df7e0ff474d3c2b5ac0
lipengzhou <lpzmail@163.com>
2019-09-18 17:56:28 +0800

    登录-表单验证-完成

--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import router from './router'
 import store from './store'
 import './styles/index.less'
 
+import {
+  ValidationProvider,
+  extend,
+  ValidationObserver
+} from 'vee-validate'
+import * as rules from 'vee-validate/dist/rules' // 加载所有的验证规则
+import zhCN from 'vee-validate/dist/locale/zh_CN' //
+
 // 按需注册 Vant 组件
 import {
   Button,
@@ -21,6 +29,25 @@ Vue
   .use(NavBar)
   .use(Field)
   .use(Toast)
+
+// loop over all rules
+for (let rule in rules) {
+  extend(rule, {
+    ...rules[rule], // add the rule
+    message: zhCN.messages[rule] // add its message
+  })
+}
+
+// 一个原则：有内置的使用内置的，内置的满足不了再自定义
+extend('phone', {
+  validate (value) {
+    return /^1\d{10}$/.test(value)
+  },
+  message: '必须是手机号'
+})
+
+Vue.component('ValidationProvider', ValidationProvider)
+Vue.component('ValidationObserver', ValidationObserver)
 
 Vue.config.productionTip = false
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,24 +4,48 @@
     <van-nav-bar title="登录" />
     <!-- /导航栏 -->
 
-    <!-- 登录表单 -->
-    <van-cell-group>
-      <van-field
-        v-model="user.mobile"
-        required
-        clearable
-        label="手机号"
-        placeholder="请输入手机号"
-      />
+    <!--
+      ValidationProvider 是验证插件提供的一个全局组件
+        rules="secret" 配置验证规则
+        v-slot="{ errors }" 获取验证相关的结果参数
+        errors[0] 用来获取验证失败的错误消息
+     -->
+    <!-- <ValidationProvider rules="required|email" name="手机号" v-slot="{ errors }">
+      <input v-model="user.mobile" type="text">
+      <span>{{ errors[0] }}</span>
+    </ValidationProvider> -->
 
-      <van-field
-        v-model="user.code"
-        type="password"
-        label="验证码"
-        placeholder="请输入验证码"
-        required
-      />
-    </van-cell-group>
+    <!-- 登录表单 -->
+    <!--
+      ValidationObserver 组件会渲染成一个 form 表单
+      可以通过 ref 调用这个组件的方法：validate 来进行 js 验证
+     -->
+    <ValidationObserver tag="form" ref="loginForm">
+      <!--
+        把需要验证的字段使用 ValidationProvider 包裹起来
+        在其上面配置对应的验证规则等信息
+       -->
+      <ValidationProvider tag="div" name="手机号" rules="required|phone" v-slot="{ errors }">
+        <van-field
+          v-model="user.mobile"
+          required
+          clearable
+          label="手机号"
+          placeholder="请输入手机号"
+          :error-message="errors[0]"
+        />
+      </ValidationProvider>
+      <ValidationProvider tag="div" name="验证码" rules="required" v-slot="{ errors }">
+        <van-field
+          v-model="user.code"
+          type="password"
+          label="验证码"
+          placeholder="请输入验证码"
+          required
+          :error-message="errors[0]"
+        />
+      </ValidationProvider>
+    </ValidationObserver>
     <!-- /登录表单 -->
     <!-- 登录按钮 -->
     <div class="login-wrap">
@@ -48,10 +72,19 @@ export default {
 
   methods: {
     async onLogin () {
-      this.isLoginLoading = true
       // 获取表单数据
       // 发送请求
       try {
+        // 表单验证
+        const isValid = await this.$refs.loginForm.validate()
+
+        // 验证失败，则什么都不做
+        if (!isValid) {
+          return
+        }
+
+        // 验证通过，提交表单
+        this.isLoginLoading = true
         const { data } = await login(this.user)
 
         console.log(data)
