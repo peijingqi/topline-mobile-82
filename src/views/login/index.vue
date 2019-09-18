dd5d68767a1330fa608ebef0117f27c11e97ad55
lipengzhou <lpzmail@163.com>
2019-09-18 16:10:12 +0800

    登录-基本流程

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,6 +7,7 @@
     <!-- 登录表单 -->
     <van-cell-group>
       <van-field
+        v-model="user.mobile"
         required
         clearable
         label="手机号"
@@ -14,6 +15,7 @@
       />
 
       <van-field
+        v-model="user.code"
         type="password"
         label="验证码"
         placeholder="请输入验证码"
@@ -23,14 +25,39 @@
     <!-- /登录表单 -->
     <!-- 登录按钮 -->
     <div class="login-wrap">
-      <van-button type="info">登录</van-button>
+      <van-button type="info" @click="onLogin">登录</van-button>
     </div>
   </div>
 </template>
 
 <script>
+import request from '@/utils/request'
+
 export default {
-  name: 'LoginIndex'
+  name: 'LoginIndex',
+  data () {
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
+  },
+
+  methods: {
+    async onLogin () {
+      // 获取表单数据
+      // 发送请求
+      const { data } = await request({
+        method: 'POST',
+        url: '/app/v1_0/authorizations',
+        data: this.user
+      })
+
+      console.log(data)
+      // 根据结果进行后续处理
+    }
+  }
 }
 </script>
 
